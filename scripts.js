058b8f47c277a1d9179d7404f831e3b913bcbddb
Juan Manuel Giulietti <juanmanuelgiulietti2@gmail.com>
2026-01-08 15:48:54 -0300

    Cambio las variables y los IDs a idioma Ingles. Modifico la funcion agregar tarea.

--- scripts.js
+++ scripts.js
@@ -1,14 +1,30 @@
-const input = document.getElementById('input');
-const agregar = document.getElementById('agregar');
-const lista = document.getElementById('lista');
+const task = document.getElementById("task");
+const add = document.getElementById("add");
+const list = document.getElementById("taskList");
 
-function agregarTarea(){
-    const tarea = input.value;
-    if(tarea !== ''){
-        const item = document.createElement('li');
-        item.textContent = tarea;
-        document.getElementById('lista').appendChild(item);
-    } else {
-        alert('Por favor ingresa una tarea válida.');
-    }
-};
+function agregarTarea() {
+  const taskContent = task.value;
+
+  if (taskContent !== "") {
+    /* Creo variables con los elementos que quiero agregar al añadir una tarea*/
+    const item = document.createElement("li");
+    item.textContent = taskContent;
+    const inputCheck = document.createElement("input");
+    inputCheck.type = "checkbox";
+    const remove = document.createElement("button");
+    remove.textContent = "Eliminar";
+
+    /* Asigno la función de eliminación al botón */
+    remove.onclick = function () {
+      list.removeChild(item);
+      list.removeChild(inputCheck);
+    };
+
+    /* Agrego los elementos al DOM */
+    document.getElementById("taskList").appendChild(inputCheck);
+    document.getElementById("taskList").appendChild(item);
+    document.getElementById("taskList").appendChild(remove);
+  } else {
+    alert("Por favor ingresa una tarea válida.");
+  }
+}
